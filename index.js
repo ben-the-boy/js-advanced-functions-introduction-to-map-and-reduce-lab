5c20b6c03fd34dcaedf5eb5c3ee90535153dbe8c
Ben Hayes <b.t.hayes@comcast.net>
2021-07-14 17:03:45 -0500

    commit tree 15ba7ddeeb7591d7393b92e0293cfc606987fa40

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(number => newArray.push(number * -1));
+  return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(number => newArray.push(number));
+  return newArray;
+}
+
+function mapToDouble(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(number => newArray.push(number * 2));
+  return newArray;
+}
+
+function mapToSquare(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(number => newArray.push(number ** 2));
+  return newArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let base = startingPoint;
+  sourceArray.forEach(number => base = base + number);
+  return base;
+}
+
+function reduceToAllTrue(sourceArray) {
+  let newArray = [];
+  for (const element of sourceArray) {
+    if (element) {
+      newArray.push(element);
+    }
+  }
+  if (newArray.length == sourceArray.length) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let newArray = [];
+  for (const element of sourceArray) {
+    if (element) {
+      newArray.push(element);
+    }
+  }
+  if (newArray.length > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
